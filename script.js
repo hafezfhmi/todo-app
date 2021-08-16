e9ffcebeade198c9b3f6ec6b849ab3bdc87680eb
hafezfhmi <hafezfahmi2000@gmail.com>
2021-08-16 18:35:34 +0800

    Added delete button functionality and apply line-through when clicked on the list

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 // https://interactjs.io/
 
 // get circle button
-let btn = document.getElementsByClassName('list__circle');
+let btn = document.getElementsByClassName('list__item');
 
 // use spread operator and forEach to apply onclick event targeting this (current) element
 [...btn].forEach((curr) => {
   curr.onclick = function () {
-    this.classList.toggle('list__circle--active');
+    this.children[0].classList.toggle('list__circle--active');
+    this.children[1].classList.toggle('list__desc--line-through');
+  };
+});
+
+let del = document.getElementsByClassName('list__cross');
+
+[...del].forEach((curr) => {
+  curr.onclick = function () {
+    this.parentElement.remove();
   };
 });
