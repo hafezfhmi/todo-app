72d6e791e8017c6049cae7c9e7433822c0aa152e
hafezfhmi <hafezfahmi2000@gmail.com>
2021-08-16 17:39:44 +0800

    Added circle button click basic functionality and changed the structure of the list item for easier DOM manipulation

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // For drag and drop, try to use this library
 // https://interactjs.io/
+
+// get circle button
+let btn = document.getElementsByClassName('list__circle');
+
+// use spread operator and forEach to apply onclick event targeting this (current) element
+[...btn].forEach((curr) => {
+  curr.onclick = function () {
+    this.classList.toggle('list__circle--active');
+  };
+});
